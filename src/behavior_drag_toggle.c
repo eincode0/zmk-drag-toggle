b06f037e951e7c9cee228bde70b8616e4fc048fa
eincode0 <ein.ein@icloud.com>
2026-01-11 21:01:50 +0900

    Update behavior_drag_toggle.c

--- src/behavior_drag_toggle.c
+++ src/behavior_drag_toggle.c
@@ -1,1 +1,55 @@
+/*
+ * Drag toggle behavior for ZMK
+ */
+#define DT_DRV_COMPAT zmk_behavior_drag_toggle
 
+#include <zephyr/kernel.h>
+#include <drivers/behavior.h>
+
+#include <zmk/behavior.h>
+#include <zmk/mouse.h>
+
+struct drag_toggle_data {
+    bool active;
+};
+
+static int drag_toggle_pressed(struct zmk_behavior_binding *binding,
+                               struct zmk_behavior_binding_event event) {
+    struct drag_toggle_data *data = binding->behavior_dev->data;
+
+    data->active = !data->active;
+
+    if (data->active) {
+        zmk_mouse_button_press(MOUSE_BUTTON_LEFT);
+    } else {
+        zmk_mouse_button_release(MOUSE_BUTTON_LEFT);
+    }
+
+    return ZMK_BEHAVIOR_OPAQUE;
+}
+
+static int drag_toggle_released(struct zmk_behavior_binding *binding,
+                                struct zmk_behavior_binding_event event) {
+    return ZMK_BEHAVIOR_OPAQUE;
+}
+
+static const struct behavior_driver_api drag_toggle_api = {
+    .binding_pressed = drag_toggle_pressed,
+    .binding_released = drag_toggle_released,
+};
+
+static int drag_toggle_init(const struct device *dev) {
+    struct drag_toggle_data *data = dev->data;
+    data->active = false;
+    return 0;
+}
+
+DEVICE_DT_INST_DEFINE(0,
+    drag_toggle_init,
+    NULL,
+    &(struct drag_toggle_data){},
+    NULL,
+    APPLICATION,
+    CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
+    &drag_toggle_api
+);
